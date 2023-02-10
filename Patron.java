34814542824f4a066d82150fa7ab32c3d0916f57
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-02-10 11:56:15 -0600

    cambios

---
+++ Patron.java
@@ -0,0 +1,12 @@
+
+/*Programo uso del patrón, Factory Patron */
+import java.util.Scanner;
+
+public class Patron {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+
+
+
+    }}
