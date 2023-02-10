9e4c7258f6fb959f1cecbf6a4207bbe5b39c1c1b
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-02-10 12:00:00 -0600

    coffee class

--- cafes.java
+++ cafes.java
@@ -1,3 +1,59 @@
 public class cafes {
+    public String expresso;
+    public String americano;
+    public String latte;
+    public String cappuccino;
+    public String helado;
+    public String frappe;
+    public String caramel_macchiato;
+    public String moca;
     
+    public String getExpresso() {
+        return expresso;
+    }
+    public void setExpresso(String expresso) {
+        this.expresso = expresso;
+    }
+    public String getAmericano() {
+        return americano;
+    }
+    public void setAmericano(String americano) {
+        this.americano = americano;
+    }
+    public String getLatte() {
+        return latte;
+    }
+    public void setLatte(String latte) {
+        this.latte = latte;
+    }
+    public String getCappuccino() {
+        return cappuccino;
+    }
+    public void setCappuccino(String cappuccino) {
+        this.cappuccino = cappuccino;
+    }
+    public String getHelado() {
+        return helado;
+    }
+    public void setHelado(String helado) {
+        this.helado = helado;
+    }
+    public String getFrappe() {
+        return frappe;
+    }
+    public void setFrappe(String frappe) {
+        this.frappe = frappe;
+    }
+    public String getCaramel_macchiato() {
+        return caramel_macchiato;
+    }
+    public void setCaramel_macchiato(String caramel_macchiato) {
+        this.caramel_macchiato = caramel_macchiato;
+    }
+    public String getMoca() {
+        return moca;
+    }
+    public void setMoca(String moca) {
+        this.moca = moca;
+    }
 }
